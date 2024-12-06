dc7f88c37852c0ffa5665cd9832a91db8039fa28
Márcio DEV <playdznvlt@gmail.com>
2024-12-05 22:57:58 -0300

    feat: Add NavbarTab component

--- apps/client/components/navbar/Navbar.vue
+++ apps/client/components/navbar/Navbar.vue
@@ -1,3 +1,7 @@
+<script lang="ts" setup>
+const { t } = useI18n();
+</script>
+
 <style lang="scss" scoped>
 @use '~/assets/scss/components/navbar/navbar.scss';
 </style>
@@ -5,7 +9,14 @@
 <template>
 	<nav>
 		<div class="navbar-container">
-			<div class="navbar-container-start"></div>
+			<div class="navbar-container-start">
+				<NavbarTab to="/">
+					<Icon size="32" name="lucide:home" />
+				</NavbarTab>
+				<NavbarTab to="/developers">{{ t('components.navbar.tabs.developers') }}</NavbarTab>
+				<NavbarTab to="/media">{{ t('components.navbar.tabs.media') }}</NavbarTab>
+				<NavbarTab to="/about">{{ t('components.navbar.tabs.about') }}</NavbarTab>
+			</div>
 
 			<div class="navbar-container-center">
 				<NuxtLinkLocale to="/">
---
+++ apps/client/components/navbar/Tab.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+const props = defineProps<{
+	to?: string;
+}>();
+</script>
+
+<style lang="scss" scoped>
+@use '~/assets/scss/components/navbar/tab.scss';
+</style>
+
+<template>
+	<NuxtLinkLocale :to="props.to" is="div" class="navbar-tab" active-class="active">
+		<slot />
+	</NuxtLinkLocale>
+</template>
