e85118d2b4b14f2371a0f2b667ecd0524af05460
Márcio DEV <playdznvlt@gmail.com>
2025-01-02 10:33:44 -0300

    feat: Add bcrypt worker

--- apps/workers/src/workers/BcryptGenerator.ts
+++ apps/workers/src/workers/BcryptGenerator.ts
@@ -1,7 +1,18 @@
+import bcrypt from 'bcrypt';
+
 import { BaseWorker } from './Base';
 
 export class BcryptGeneratorWorker extends BaseWorker {
-	static async execute(data?: any) {
-		console.log('BcryptWorker executed');
+	static async execute(data?: { password?: string; rounds?: number }) {
+		if (!data?.password) {
+			throw new Error('password-required');
+		}
+
+		const rounds = data.rounds || 10;
+
+		const salt = await bcrypt.genSalt(rounds);
+		const hash = await bcrypt.hash(data.password, salt);
+
+		return { hash };
 	}
 }
