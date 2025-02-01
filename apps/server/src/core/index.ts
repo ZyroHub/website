cb03c551b71b2dea4070347392e1677d5686cffc
Márcio DEV <playdznvlt@gmail.com>
2025-02-01 17:44:42 -0300

    feat: implement clustering for server and worker processes with error handling

--- apps/server/src/core/index.ts
+++ apps/server/src/core/index.ts
@@ -1,8 +1,36 @@
-import { ServerModule } from '@/modules/index.js';
+import cluster from 'node:cluster';
+import os from 'node:os';
+import process from 'node:process';
+import ansicolor from 'ansicolor';
+
+import { Terminal } from '@zyrohub/toolkit';
+
+import { ServerModule } from '@/modules/modules.js';
 import { Core } from './Core.js';
 
-const core = new Core();
+if (cluster.isPrimary) {
+	Terminal.success('CLUSTER', `Primary ${ansicolor.cyan(process.pid)} is running!`);
 
-core.init();
+	const cpuCount = os.availableParallelism();
+
+	for (let i = 0; i < cpuCount; i++) {
+		cluster.fork();
+	}
+
+	cluster.on('exit', (worker, code, signal) => {
+		Terminal.error(
+			'CLUSTER',
+			`Worker ${ansicolor.cyan(worker.process.pid)} died with code: ${ansicolor.yellow(
+				code
+			)}, and signal: ${ansicolor.red(signal)}`
+		);
+	});
+} else {
+	Terminal.success('CLUSTER', `Worker ${ansicolor.cyan(process.pid)} started!`);
+
+	const core = new Core();
+
+	core.init();
+}
 
 export type App = (typeof ServerModule)['server'];
