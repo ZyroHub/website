3636b7eb7541afd97dff9e25d1d3ab73c9d54871
Márcio DEV <playdznvlt@gmail.com>
2025-02-06 20:43:41 -0300

    feat: Improve Buy Me a Coffee button styles and change component to button element

--- apps/client/components/button/BuyMeACoffee.vue
+++ apps/client/components/button/BuyMeACoffee.vue
@@ -14,14 +14,14 @@ const { data: contributorsData } = await useFetch('/api/contributors', {});
 			v-if="runtimeConfig.public.buy_me_a_coffee_slug"
 			:href="`https://www.buymeacoffee.com/${runtimeConfig.public.buy_me_a_coffee_slug}`"
 			target="_blank">
-			<Button class="button-bmac">
+			<button class="button-bmac">
 				<span><NuxtImg src="images/bmc-logo.webp" /> Buy me a coffee</span>
 
 				<div>
 					<Icon name="ri:heart-fill" :size="16" />
 					<p>{{ contributorsData.buy_me_a_coffee_supporters.total || 0 }}</p>
 				</div>
-			</Button>
+			</button>
 		</a>
 	</div>
 </template>
