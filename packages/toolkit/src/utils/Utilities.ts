a7e60b07f3d4257eb96c6425794de74c4478e960
Márcio DEV <playdznvlt@gmail.com>
2025-05-27 20:37:27 -0300

    feat: add utility methods for generating random strings with customizable options

--- packages/toolkit/src/utils/Utilities.ts
+++ packages/toolkit/src/utils/Utilities.ts
@@ -1,1 +1,37 @@
-export class Utilities {}
+export class Utilities {
+	static chars = {
+		upper: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+		lower: 'abcdefghijklmnopqrstuvwxyz',
+		number: '0123456789',
+		special: '!@#$%^&*()_+[]{}|;:,.<>?'
+	};
+
+	static get allChars() {
+		return `${this.chars.upper}${this.chars.lower}${this.chars.number}${this.chars.special}`;
+	}
+
+	static get alphanumericChars() {
+		return `${this.chars.upper}${this.chars.lower}${this.chars.number}`;
+	}
+
+	static async generateRandomString(options: {
+		length?: number;
+		chars?: string;
+		include?: string;
+		exclude?: string;
+	}) {
+		const charList = `${options.chars || this.allChars}${options.include}`.replace(
+			new RegExp(`[${options.exclude}]`, 'g'),
+			''
+		);
+		const length = options.length || 16;
+
+		let randomString = '';
+		for (let i = 0; i < length; i++) {
+			const randomIndex = Math.floor(Math.random() * charList.length);
+			randomString += charList[randomIndex];
+		}
+
+		return randomString;
+	}
+}
