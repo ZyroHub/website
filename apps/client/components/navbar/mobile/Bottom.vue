6500e5c613020631792ed87329d364295c7078cc
Márcio DEV <playdznvlt@gmail.com>
2025-06-18 19:07:41 -0300

    feat: implement mobile navbar with dynamic tabs and update UnoCSS configuration

--- apps/client/components/navbar/mobile/Bottom.vue
+++ apps/client/components/navbar/mobile/Bottom.vue
@@ -1,13 +1,23 @@
-<script lang="ts" setup></script>
-
-<style lang="scss" scoped>
-@use '~/assets/scss/components/navbar/mobile/bottom.scss';
-</style>
+<script lang="ts" setup>
+const { t } = useI18n();
+</script>
 
 <template>
-	<nav class="navbar-bottom">
-		<div class="flex">
-		
+	<nav class="w-full fixed bottom-0 z-100 bg-black-900 dark:bg-black-950 left-0">
+		<div class="flex w-full justify-around items-center h-16">
+			<NavbarMobileTab to="/" :title="t('components.navbar.tabs.home')">
+				<Icon :size="30" name="lucide:home" />
+			</NavbarMobileTab>
+			<NavbarMobileTab to="/developers" :title="t('components.navbar.tabs.developers')">
+				<Icon :size="30" name="ant-design:code-filled" />
+			</NavbarMobileTab>
+			<NavbarMobileTab to="/creators" :title="t('components.navbar.tabs.creators')">
+				<Icon :size="30" name="ant-design:camera-filled" />
+			</NavbarMobileTab>
+			<NavbarMobileTab to="/misc" :title="t('components.navbar.tabs.misc')">
+				<Icon :size="30" name="jam:tools-f" />
+			</NavbarMobileTab>
+			<NavbarMobileTab to="/about"><Icon :size="30" name="material-symbols:info-rounded" /></NavbarMobileTab>
 		</div>
 	</nav>
 </template>
---
+++ apps/client/components/navbar/mobile/Tab.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+const props = defineProps<{
+	to?: string;
+	title?: string;
+}>();
+</script>
+
+<template>
+	<NuxtLinkLocale
+		:to="props.to"
+		:aria-label="props.title"
+		is="div"
+		class="text-neutral-200 w-full flex justify-center rounded-t-xl duration-200 [&.active]:text-black-900 [&.active]:bg-primary-50 [&.active]:dark:text-neutral-50 [&.active]:dark:bg-primary-400 h-full items-center dark:text-neutral-200 decoration-none"
+		active-class="active">
+		<slot />
+	</NuxtLinkLocale>
+</template>
