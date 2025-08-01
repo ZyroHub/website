c8c8ce17339821fdca41908cd644f04946288bef
Márcio DEV <playdznvlt@gmail.com>
2025-08-01 12:13:30 -0300

    feat: update Buy Me a Coffee button styles and enhance Discord embed component with improved text colors

--- apps/client/components/discord/Embed.vue
+++ apps/client/components/discord/Embed.vue
@@ -11,17 +11,17 @@ const props = defineProps<{
 	<div>
 		<DiscordContainer>
 			<div class="flex justify-between gap-4">
-				<div>
+				<div>3
 					<DiscordContent
 						v-if="props.embed.title"
 						:content="props.embed.title"
-						class="font-semibold"
+						class="font-semibold text-neutral-50"
 						disableMarkdown />
 
 					<DiscordContent
 						v-if="props.embed.description"
 						:content="props.embed.description"
-						class="text-[14px] mt-2" />
+						class="text-[14px] mt-2 text-neutral-200" />
 				</div>
 
 				<div v-if="props.embed.thumbnail">
