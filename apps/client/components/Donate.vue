4cef46023680b26803180d1fe6378f558b16e898
Márcio DEV <playdznvlt@gmail.com>
2025-10-16 21:09:30 -0300

    feat: update Donate component to conditionally render donation link based on runtime config

--- apps/client/components/Donate.vue
+++ apps/client/components/Donate.vue
@@ -1,5 +1,18 @@
+<script lang="ts" setup>
+const runtimeConfig = useRuntimeConfig();
+</script>
+
 <template>
 	<div class="fixed bottom-16 right-4">
-		<Button class="bg-primary-300 hover:bg-primary-400">Support us on <NuxtImg src="images/bmc-logo.webp" class="w-4 ml-2" /></Button>
+		<a
+			v-if="runtimeConfig.public.buy_me_a_coffee_slug"
+			:href="`https://www.buymeacoffee.com/${runtimeConfig.public.buy_me_a_coffee_slug}`"
+			target="_blank"
+			:draggable="false"
+			class="button-bmac-link">
+			<Button class="bg-primary-300 hover:bg-primary-400">
+				Support us on <NuxtImg src="images/bmc-logo.webp" class="w-4 ml-2" />
+			</Button>
+		</a>
 	</div>
 </template>
